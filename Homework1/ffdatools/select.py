7860f65c37f8f0dd76954257b731ad5c8cbe6ba4
LoZimmero <fiorettigabriele@gmail.com>
2023-06-05 14:54:07 +0200

    Added .gitignore + Added logs in select.py

--- Homework1/ffdatools/select.py
+++ Homework1/ffdatools/select.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser, Namespace
+from datetime import datetime
 
 def is_line_of_node(line: str, node: str):
     try:
@@ -12,12 +13,18 @@ def main(args: Namespace):
     node_name = args.node_name
     output_file = args.output_file or f'{node_name}_output'
 
+    log_info('Starting selection of recors of node {} from file {}'.format(node_name, source_file))
+
     file_lines = open(source_file).readlines()
     with open(output_file, 'w') as f:
         for line in file_lines:
             if is_line_of_node(line, node_name):
                 f.write(line)
 
+    log_info('Finished. Output saved in file {}'.format(output_file))
+
+def log_info(message: str):
+    print(f'[INFO] - {datetime.now()} - {message}')
 
 if __name__ == '__main__':
     ap = ArgumentParser()
