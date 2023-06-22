e63504e940e5dbdc3e311f2c40dff6e7ccb37fa2
S-Federico <fondercoast@gmail.com>
2023-06-22 18:34:06 +0200

    updated frequent itemsets code

--- Homework1/frequent_itemsets.R
+++ Homework1/frequent_itemsets.R
@@ -1,10 +1,25 @@
 ## Apriori
 library("arules")
 library("arulesViz")
+# Get current file path
+CURR_FILE_PATH <- getwd()
 
-data<-read.transactions("C:\\Users\\fonde\\Documents\\GitHub\\DataScienceHomeworks\\Homework1\\ffdatools\\tuplessept.txt ",sep = ",")
-rules<-apriori(data, parameter = 
+#solo per il pc di federico
+CURR_FILE_PATH<-paste0(CURR_FILE_PATH,"/GitHub/DataScienceHomeworks/Homework1")
+
+
+# method paste() to join strings. MUST use sep="" to avoid adding empty space between strings
+FFDATOOLS <- paste(CURR_FILE_PATH, "/ffdatools", sep="")
+
+datasept<-read.transactions(paste0(FFDATOOLS,"/transactions_sept.txt"),sep = ",")
+dataoct<-read.transactions(paste0(FFDATOOLS,"/transactions_oct.txt"),sep = ",")
+
+rulessept<-apriori(datasept, parameter = 
                  list(support=0.25, 
                       minlen=2,maxlen=10, target="frequent itemsets")) 
-inspect(sort(rules, by="support"))
+inspect(sort(rulessept, by="support"))
 
+rulesoct<-apriori(dataoct, parameter = 
+                 list(support=0.25,
+                      minlen=2,maxlen=10, target="frequent itemsets")) 
+inspect(sort(rulesoct, by="support"))
